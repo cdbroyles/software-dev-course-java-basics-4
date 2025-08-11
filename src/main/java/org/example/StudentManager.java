8a1ab52220e84ca2f1bf02f7ba54b847a31263b7
cdbroyles <cdbroyles1986@gmail.com>
2025-08-11 13:31:24 -0500

    finished optional part of assignment

--- src/main/java/org/example/StudentManager.java
+++ src/main/java/org/example/StudentManager.java
@@ -35,7 +35,9 @@ public class StudentManager {
         // Prompt the user to enter a new student name (using scanner and I/O methods learned previously,
         // refer to mainMenu() for an example)
         // Add the student to the list
-
+        System.out.println("Please enter the student's name to add: ");
+        String name = scanner.nextLine();
+        students.add(name);
         mainMenu();
     }
 
@@ -43,13 +45,18 @@ public class StudentManager {
         // Prompt the user for a student name
         // Use the contains method to check if the student entered is in the list
         // If so, remove it, if not, print "Student not found."
-
+        System.out.println("Please enter the student's name to remove: ");
+        String name = scanner.nextLine();
+        students.remove(name);
         mainMenu();
     }
 
     public void viewStudents() {
         // Loop through the list of students and print each one
         // (Use a for-each loop!)
+        for (String student : students) {
+            System.out.println(student);
+        }
         mainMenu();
     }
 }
